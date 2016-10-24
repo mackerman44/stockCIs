72a2f467564e4136e0a4379c934bf73d2cee54ca
Mike Ackerman <mackerman@qcinc.org>
2016-10-24 11:11:51 -0600

    Started resampit() function

---
+++ R/resampit.R
@@ -0,0 +1,13 @@
+#' @title resampit: Simple bootstrapping of fish stocks to generate CIs
+#'
+#' @description Bootstrap stock assignments to generate CIs
+#'
+#' @param
+#'
+#' @author Mike Ackerman
+#'
+#' @export
+#' @return NULL
+
+
+
