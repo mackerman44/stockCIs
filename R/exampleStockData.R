d58cbe87ba58f191d4873132ad1f98aa3fd85b2e
Mike Ackerman <mackerman@qcinc.org>
2016-10-24 11:42:11 -0600

    Added exampleStockData. Cleaned up initial roxygen documentation

---
+++ R/exampleStockData.R
@@ -0,0 +1,11 @@
+#' @title Example of Stock Assignments for resampit()
+#'
+#' @description An example of stock assignment data to be bootstrapped using resampit() or bootstock(). This example
+#' data is from parentage-based tagging (PBT).
+#'
+#' @name exampleStockData
+#' @docType data
+#' @usage exampleStockData
+#' @format A data frame. Must contain a column named 'stock' containing the stock assignments.
+#' @keywords stock data
+NULL
--- R/resampit.R
+++ R/resampit.R
@@ -2,14 +2,18 @@
 #'
 #' @description Bootstrap stock assignments to generate CIs
 #'
-#' @param
+#' @param stockAssignments description here
+#' @param tagRates description here
+#' @param bootstraps The number of bootstraps to be performed
+#' @param ci The confidence interval to be generated
+#' @param abundance If abundance results are desired, enter the estimated abundance
 #'
 #' @author Mike Ackerman
 #'
 #' @export
 #' @return NULL
 
-resampit <- function(pbtAssignments = NULL, pbtTagRate = NULL, bootstraps = 500, ci = 0.90, abundance = NULL)
+resampit <- function(stockAssignments = NULL, tagRates = NULL, bootstraps = 500, ci = 0.90, abundance = NULL)
 {
 
 
