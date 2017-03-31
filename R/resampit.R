b5e3c56c271f871bf965de3682c9c46fc20700a9
Mike Ackerman <mackerman@qcinc.org>
2017-03-31 10:16:13 -0600

    Starting version 2 of resampit

--- R/resampit.R
+++ R/resampit.R
@@ -1,107 +1,36 @@
-#' @title resampit: Simple bootstrapping of fish stocks to generate CIs
+#' @title resampit: Simple bootstrapping of fish groups to generate CIs
 #'
-#' @description Bootstrap stock assignments to generate CIs
+#' @description resampit() is a function that re-samples (bootstraps) a table of group assignments (e.g., from PBT
+#' GSI, PIT tags, etc.) and then expands those groups according to group-specific tag rates to generate
+#' confidence intervals around group proportion estimates.
 #'
-#' @param stockAssignments description here
-#' @param tagRates description here
-#' @param bootstraps The number of bootstraps to be performed
-#' @param ci The confidence interval to be generated
+#' @param assignmentFile The .csv file containing the group (e.g, PBT) assignments including those fish that could not
+#' be assigned to a group.
+#' @param assignmentColumn The name of the column in \code{assignmentFile} containing the group assignments. In the
+#' case of PBT, this column should contain the hatchery and brood year (BY) combination for any assigned fish.
+#' For example, OtsRAPH08 may represent a fish that assigns to Rapid River hatchery broodstock spawned in 2008.
+#' Note that every group defined in this column MUST have a tagging rate defined in the \code{tagRates} file (see
+#' below). Otherwise, an error will print (e.g., "OtsRAPH08 has no tagging rate defined") and assignments for that
+#' group will NOT be expanded. Fish that did not assign to a group MUST contain "Unassigned" in this column.
+#' @param tagRates The .csv file containing tagging rates for any group defined in the \code{assignmentColumn} column of
+#' \code{assignmentFile}. The file should only contain 2 columns. The first column should contain group names that
+#' MUST match the group names used in the \code{assignmentColumn} column of \code{assignmentFile}. Tagging rates must
+#' be expressed as proportions.
+#' @param bootstraps The number of bootstraps to be performed.
+#' @param ci The confidence interval to be generated expreseed as a proportion (e.g., 0.90 or 0.95).
 #'
 #' @author Mike Ackerman
 #'
 #' @export
 #' @return NULL
 
-resampit <- function(stockAssignments = NULL, tagRates = NULL, bootstraps = 500, ci = 0.90)
-{
-  # READ IN DATA
-  if(is.character(stockAssignments) == TRUE) { stockAssignments <- read.csv(file = stockAssignments, header = TRUE, na.strings = c("NA","")) } else { stockAssignments <- stockAssignments }
-  if(is.character(tagRates) == TRUE) { tagRates <- read.csv(file = stockAssignments, header = TRUE, na.strings = c("NA","")) } else { tagRates <- tagRates }
+resampit <- function(assignmentFile = NULL, assignmentColumn = "groups", tagRates = NULL,
+                     bootstraps = 500, ci = 0.90)
 
-  sampleSize <- nrow(stockAssignments)
-  stocks     <- unique(stockAssignments$stock)
 
-  # Create a blank data frame to store results from each bootstrap of stock assignments
-  freqTable        <- as.data.frame(matrix(rep(NA,(bootstraps*length(stocks))),bootstraps,length(stocks)))
-  names(freqTable) <- stocks
-
-  # Bootstrap loop to re-sample the 'stock' column the desired number of bootstraps
-  for(b in 1:bootstraps)
-  {
-    boot <- sample(x = as.character(stockAssignments$stock), size = sampleSize, replace = TRUE)
-    freqTable[b,] <- lapply(as.character(stocks), function(x) length(boot[boot==x]))
-  }
-  # END BOOTSTRAP LOOP
-
-  # stocks within the freqTable now need to be expanded according to each stock's tag rate defined in the tagRates.
-  # Here we set up a new blank table to store results after frequencies within freqTable are expanded
-  # If no tagging rate is defined (or codes don't match), an error will occur
-  expansionTable <- array(NA, dim = dim(freqTable), dimnames = list(1:bootstraps, colnames(freqTable)))
-  for(stock in as.character(stocks[-match("Unassigned", stocks)]))
-  {
-    if(stock %in% tagRates[,"stock"])
-    {
-      expansionTable[,stock] <- as.numeric(freqTable[,stock]) / tagRates[as.character(tagRates[,1]) == stock,2]
-    } else
-    {
-      print(paste(stock, "has no tagging rate defined in tagRates"))
-    }
-  } # END EXPANSION LOOP
-  # Now we need to reduce the 'Unassigned' column by the amount that all stocks were expanded
-  expansionTable[,"Unassigned"] <- sampleSize - apply(expansionTable[,-match("Unassigned", stocks)], 1, sum)
-
-  # This is a correction in the case where Unassigned < 0 after correcting for tagging rates
-  for (i in 1:nrow(expansionTable))
-  {
-    if (expansionTable[i,"Unassigned"] < 0)
-    {
-      correction <- (expansionTable[i,-which(colnames(expansionTable) == "Unassigned")] / sum(expansionTable[i,-which(colnames(expansionTable) == "Unassigned")])) * abs(expansionTable[i,"Unassigned"])
-      expansionTable[i,-which(colnames(expansionTable) == "Unassigned")] <- expansionTable[i,-which(colnames(expansionTable) == "Unassigned")] - correction
-      expansionTable[i,"Unassigned"] <- 0
-    }
-  }
-  ### END CORRECTION LOOP
-
-  # Now we want to create a table of proportions...the 'expansionTable' is divided by the total sample size
-  propTable <- expansionTable / sampleSize
-
-  # Finally, we need to generate the point estimates from the original data
-  expandedFreq <- array(NA, dim = c(1,length(stocks)), dimnames = list("pointEstimate", stocks))
-  stockByFreq  <- table(stockAssignments$stock)
-  for(stock in as.character(stocks[-match("Unassigned", stocks)]))
-  {
-    if(stock %in% tagRates[,1])
-    {
-      expandedFreq[1,stock] <- stockByFreq[stock] / tagRates[as.character(tagRates[,1]) == stock,2]
-    }
-  }
-  expandedFreq[1,"Unassigned"] <- sampleSize - sum(expandedFreq[1, as.character(stocks[-match("Unassigned", stocks)])])
-
-  ### Correction of point estimates IF Unassigned < 0
-  if(expandedFreq[1,"Unassigned"] < 0)
-  {
-    pointCorrect <- (expandedFreq[1,-which(colnames(expandedFreq) == "Unassigned")] / sum(expandedFreq[1,-which(colnames(expandedFreq) == "Unassigned")])) * abs(expandedFreq[1,"Unassigned"])
-    expandedFreq[1,-which(colnames(expandedFreq) == "Unassigned")] <- expandedFreq[1,-which(colnames(expandedFreq) == "Unassigned")] - pointCorrect
-    expandedFreq[1,"Unassigned"] <- 0
-  }
-  # END OF POINT ESTIMATE CORRECTION
-
-  # Convert point extimates to proportions
-  pointEstimates <- expandedFreq/sampleSize
-
-  # Now let's generate some summary statistics based on propTable (Lower CI, Upper CI, Mean of iterations,
-  # Median of iterations, standard deviation, coefficient of variation
-  lci    <- apply(propTable, 2, quantile, (1-ci)/2)
-  uci    <- apply(propTable, 2, quantile, ((1-ci)/2)+ci)
-  mean   <- apply(propTable, 2, mean)
-  median <- apply(propTable, 2, median)
-  stDev  <- apply(propTable, 2, sd)
-  se <- function(x) sd(x)/sqrt(length(x)) # Function to calculate standard error
-  stdErr <- apply(propTable, 2, se)
-  cv     <- (stDev/mean)*100
-
-  results <- cbind(t(pointEstimates), mean, median, lci, uci, stDev, stdErr, cv)
-
-  write.csv(results,   file = "results.csv")
-  write.csv(propTable, file = "propTable.csv")
-}
+#    2) A .csv file containing tagging rates
+#
+#  File must contain two columns labelled 'hatchery' and 'tag_rate'. The code used in the 'hatchery' column MUST match the code used
+#  in the 'pbt' column of 'pbt_assignments.csv'
+#
+#  IMPORTANT: Tagging rates must be expressed as a proportions (NOT as a percentage)
