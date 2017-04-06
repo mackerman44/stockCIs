5ef84576f7c91183f6d795eccc03986caeffc2d9
Mike Ackerman <mackerman@qcinc.org>
2017-04-06 10:22:29 -0600

    Started bootstock v2.0 on 4-6-2017

--- R/bootstock.R
+++ R/bootstock.R
@@ -1,144 +1,33 @@
-#' @title bootstock: Bootstrapping of PBT and GSI assignments to generate stock CIs
+#' @title bootstock: Bootstrapping of PBT and GSI assignments to generate group CIs
 #'
-#' @description Bootstrapping of PBT and GSI assignments to generate stock CIs around point estimates
+#' @description bootstock() is a function that re-samples (bootstraps) a table of PBT and GSI assignments to
+#' generate confidence intervals around group proportion estimates. Initially, bootstock() re-samples a column of 
+#' PBT and GSI assignments contained in the \code{assignmentColumn} column of \code{assignmentFile}. Then, PBT 
+#' group frequencies are 'expanded' according to group-specific tagging rates in the \code{tagRates} file. 
+#' Finally, individuals that are added to hatchery groups based on tag rate expansions must be removed from 
+#' genetically similar natural-origin stocks (i.e., the GSI assignments). These reallocations of individuals from
+#' GSI groups to PBT groups are done according to a \code{reallocateTable} provided by the user.
 #'
-#' @param stockAssignments description here
-#' @param tagRates description here
+#' @param assignmentFile The .csv file containing the group (PBT and GSI) assignments.
+#' @param assignmentColumn The name of the column in \code{assignmentFile} containing the PBT and GSI assignments.
+#' @param tagRates The .csv file containing the tagging rates for any hatchery PBT group defined in the \code{assignmentColumn}
+#' column of \code{assignmentFile}. The file should only contain 2 columns. The first column should contain group names
+#' that MUST match the hatchery group names used in the \code{assignmentColumn} column of \code{assignmentFile}. Tagging rates must
+#' be expressed as proportions.
 #' @param reallocateTable description here
-#' @param bootstraps The number of bootstraps to be performed
-#' @param ci The confidence interval to be generated
+#' @param bootstraps The number of bootstraps to be performed.
+#' @param ci The confidence interval to be generated expressed as a proportion (e.g., 0.90 or 0.95).
 #'
 #' @author Mike Ackerman
 #'
 #' @export
 #' @return NULL
 
-bootstock <- function(stockAssignments = NULL, tagRates = NULL, reallocateTable = NULL, bootstraps = 500, ci = 0.90)
+bootstock <- function(assignmentFile = NULL, assignmentColumn = "group_assignments", tagRates = NULL, 
+                      reallocateTable = NULL, bootstraps = 500, ci = 0.90)
 {
-  # READ IN DATA
-  if(is.character(stockAssignments) == TRUE) { stockAssignments <- read.csv(file = stockAssignments, header = TRUE, na.strings = c("NA","")) } else
-    { stockAssignments <- stockAssignments }
-
-  if(is.character(tagRates) == TRUE) { tagRates <- read.csv(file = stockAssignments, header = TRUE, na.strings = c("NA","")) } else
-    { tagRates <- tagRates }
-
-  if(is.character(reallocateTable) == TRUE) { reallocateTable <- read.csv(file = reallocateTable, header = TRUE, na.strings = c("NA","")) } else
-    { reallocateTable <- reallocateTable }
-
-  # GENERATE POINT ESTIMATES PRIOR TO BOOTSTRAPPING ROUTINE
-  stockFreq        <- as.data.frame(matrix(table(stockAssignments$stock),1,length(unique(stockAssignments$stock))))
-  names(stockFreq) <- names(table(stockAssignments$stock)) # Add headers (stock names) to stockFreq
-  stockNames       <- unique(stockAssignments$stock) # Create a list of all stock names in the stock column of the stockAssignments
-
-  # GENERATE LISTS OF THE HATCHERY AND 'WILD' STOCKS USING THE 'REAR' COLUMN OF stockAssignments
-  hatcheryList     <- as.character(unique(stockAssignments$stock[which(stockAssignments$rear == "H")]))
-  wildList         <- as.character(unique(stockAssignments$stock[which(stockAssignments$rear == "U")]))
-
-  # WHAT IS THE SAMPLE SIZE?
-  sampleSize <- nrow(stockAssignments)
-
-  # Below are 2 loops:
-  # 1) First, we will expand the PBT assignments based on tagging rates for the original dataset (stockAssignments)
-  # 2) Second, we will re-allocate the expanded individuals from GSI stocks (i.e., we will remove the expanded
-  # individuals from the GSI stocks based on genetic similarities between PBT and GSI groups)
-
-  expPointEsts <- stockFreq
-  for(hatcheryStock in hatcheryList)
-  {
-    if(hatcheryStock %in% tagRates[,1])
-    {
-      expPointEsts[1,hatcheryStock] <- stockFreq[hatcheryStock] / tagRates[as.character(tagRates[,1]) == hatcheryStock,2]
-    } else
-    {
-      print(paste(hatcheryStock, "has no tagging rate defined in tagRates"))
-    }
-  }
-  # END PBT EXPANSION LOOP
-
-  # Let's calculate the number of fish that were added to each PBT stock
-  expPointEstsDiff <- expPointEsts[,hatcheryList] - stockFreq[,hatcheryList]
-  # BEING GSI -> PBT REALLOCATION LOOP
-  for (hatcheryStock in hatcheryList)
-  {
-    hatcheryRelease <- as.character(unique(stockAssignments$hatchery_fish_release_location[which(stockAssignments$stock == hatcheryStock)]))
-    for (rg in wildList)
-    {
-      if(any(names(reallocateTable) == rg))
-      {
-        expPointEsts[,rg] <- expPointEsts[,rg] - (expPointEstsDiff[1,hatcheryStock] * reallocateTable[which(reallocateTable$hatchery_stock_release_location == hatcheryRelease), rg])
-      }
-    }
-  }
-  # END GSI -> PBT REALLOCATION LOOP
-
-  pointEstimates <- expPointEsts / sampleSize
-  pointEstimates <- round(pointEstimates,2)
-  # END POINT ESTIMATES
-
-  # Begin bootstrapping routine to calculate confidence intervals and CVs for each stock
-  freqTable        <- as.data.frame(matrix(rep(NA,(bootstraps*length(stockNames))), bootstraps, length(stockNames)))
-  names(freqTable) <- stockNames
-
-  # BEGIN BOOTSTRAP LOOP. This may take a couple or more minutes depending on sample size and number of bootstraps.
-  for (b in 1:bootstraps)
-  {
-    resample      <- sample(x = as.character(stockAssignments$stock), size = sampleSize, replace = TRUE)
-    freqTable[b,] <- lapply(as.character(stockNames), function(x) length(resample[resample == x]))
-  }
-  # END BOOTSTRAP LOOP
-
-  # Now, PBT stock frequencies within each bootstrap iteration must be expanded according to PBT tagging rates
-  expansionTable <- freqTable
-  # BEGIN EXPANSION LOOP ACROSS ALL ITERATIONS IN expansionTable
-  for (hatcheryStock in hatcheryList)
-  {
-    if (hatcheryStock %in% tagRates[,1])
-    {
-      expansionTable[,hatcheryStock] <- as.numeric(freqTable[,hatcheryStock]) / tagRates[as.character(tagRates[,1]) == hatcheryStock,2]
-    } else
-    {
-      print(paste(hatcheryStock, "has no tagging rate defined in tagRates"))
-    }
-  }
-  # END EXPANSION LOOP
-
-  # NOTE: At this point rows within expansionTable sum to > sampleSize. We still need to correct
-  # GSI stock frequencies
-
-  # These are the numbers that we added to each hatchery stock in each bootstrap iteration
-  expDiffTable <- expansionTable[,hatcheryList] - freqTable[,hatcheryList]
-
-  # BEGIN REALLOCATION LOOP ACROSS ALL ITERATIONS IN expansionTable
-  for (hatcheryStock in hatcheryList)
-  {
-    hatcheryRelease <- as.character(unique(stockAssignments$hatchery_fish_release_location[which(stockAssignments$stock == hatcheryStock)]))
-    for (rg in wildList)
-    {
-      if(any(names(reallocateTable) == rg))
-      {
-        expansionTable[,rg] <- expansionTable[,rg] - (expDiffTable[,hatcheryStock]*reallocateTable[which(reallocateTable$hatchery_stock_release_location == hatcheryRelease),rg])
-      }
-    }
-  }
-
-  # Create a table of stock proportions. Every value in the expansionTable is divided by the sampleSize
-  propTable  <- expansionTable / sampleSize
-
-  # Not let's generate some summary statistics from the propTable (mean, median, LCI, UCI, sd)
-  resultsBoot  <- data.frame(group = names(propTable), mean = apply(propTable, 2, mean), median = apply(propTable, 2, median),
-                             lci = apply(propTable, 2, quantile, (1-ci)/2), uci = apply(propTable, 2, quantile, ((1-ci)/2)+ci),
-                             sd = apply(propTable, 2, sd))
-  resultsBoot$CV <- (resultsBoot$sd/resultsBoot$mean) * 100
-
-  resultsPoint <- as.data.frame(t(pointEstimates))
-  resultsPoint$group <- row.names(resultsPoint)
-
-  results <- merge(resultsBoot, resultsPoint, by = "group")
-  results <- results[,c("group","V1","mean","median","lci","uci","sd","CV")]
-  print(results)
-
-  # WRITE RESULTS
-  write.csv(results, file = "results.csv")
-  write.csv(propTable, file = "stock_proportions_bootstraps.csv")
+  
 }
-# END BOOTSTOCK
+
+
+
